875af5f90ba6aa64aeb72df34063d17a1fe6f0e6
Liza21l <Liza2009koshenya@gmail.com>
2024-12-23 21:23:52 +0200

    Update text.py

--- text.py
+++ text.py
@@ -6,18 +6,19 @@ first_player = "X"
 game_over = False
 while not game_over:
     for row in board:
-        print("|". join(row)) 
+        print("|".join(row)) 
         print("_" * 5)
-
+        
     row = int(input(f"Player {first_player} enter the row (0-2):"))
     col = int(input(f"Player {first_player} enter the col (0-2):"))
     if board[row][col] == " ":
         board[row][col] = first_player
-    
+
         winner = False
         for i in range(3):
             if (board[i][0] == first_player and board[i][1] == first_player and board[i][2] == first_player) or (board[0][i] == first_player and board[1][i] == first_player and board[2][i] == first_player):
-                winner = True
+              winner = True
+
         if (board[0][0] == first_player and board[1][1] == first_player and board[2][2] == first_player) or (board[0][2] == first_player and board[1][1] == first_player and board[2][0] == first_player):
             winner = True
 
@@ -28,18 +29,19 @@ while not game_over:
 
             print(f"Player {first_player} wins!") 
             game_over = True
+
         else: 
-            draw = True 
-            for row in board: 
-                if " " in row: 
-                    draw = False 
-                if draw: 
-                row(board) 
-                print("It's a draw!") 
-                game_over = True 
-            else: 
-                first_player = "O" if first_player == "X" else "X"
-     
-else: 
-    print("This spot is already taken. Try again.")
+          draw = True 
+          for row in board: 
+              if " " in row: 
+                  draw = False 
+              if draw: 
+                  row(board) 
+                  print("It's a draw!") 
+                  game_over = True  
+
+              first_player = "O" if first_player == "X" else "X"
+  
+    else: 
+      print("This spot is already taken. Try again.")
 
